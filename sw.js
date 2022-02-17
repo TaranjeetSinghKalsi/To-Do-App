1e35525949714f7f115d9e3c3610a650bdd37d1b
Taranjeet Singh Kalsi <taranjeetkalsi15@gmail.com>
2022-02-17 23:06:06 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,24 @@
+let cacheData = 'TodoApp';
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/js/bundle.js',
+                '/index.html',
+                '/',
+                '/images/delete.svg',
+                '/images/tick.svg',
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches.match(event.request).then((resp)=>{
+            if(resp){
+                return resp;
+            }
+        })
+    )
+})
