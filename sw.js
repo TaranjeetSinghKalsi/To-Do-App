115587cf1f2ba26ad611620136f79fa7e13e8e56
Taranjeet Singh Kalsi <taranjeetkalsi15@gmail.com>
2022-02-18 15:42:13 +0530

    Updates

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('fetch', (event) => {
         caches.match(event.request)
             .then(() => {
                 return fetch(event.request) 
-                    .catch(() => caches.match('offline.html'))
+                    .catch(() => caches.match('index.html'))
             })
     )
 });
